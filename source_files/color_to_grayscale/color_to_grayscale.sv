7ff8a6973390676bef1e7195fa8f435dd13ddd28
haven chiu <haven.cy.1113@gmail.com>
2024-01-26 23:51:44 +0800

    commit

--- source_files/color_to_grayscale/color_to_grayscale.sv
+++ source_files/color_to_grayscale/color_to_grayscale.sv
@@ -20,7 +20,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module color_to_grayscale(
+module color_to_grayscale
+# (parameter SIZE = 100) (
+    input [7:0] R_in,G_in,B_in [SIZE-1:0],
+    output [7:0]grayscale_out [SIZE-1:0],
     input clk
     );
+    genvar i;
+    generate
+        for (i=1;i<SIZE-1;i=i+1) begin
+           color_to_grayscale_row(R_in[i],G_in[i],B_in[i],grayscale_out[i]);   
+        end 
+    endgenerate
 endmodule
+
