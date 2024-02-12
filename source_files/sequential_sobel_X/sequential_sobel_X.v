61fd762654573784209e08cd57ef8980307bdc94
146snk <91668969+146snk@users.noreply.github.com>
2024-02-12 16:58:40 +0800

    Sobel X changed

--- source_files/sequential_sobel_X/sequential_sobel_X.v
+++ source_files/sequential_sobel_X/sequential_sobel_X.v
@@ -24,21 +24,20 @@ module sequential_sobel_X(
     input [7:0] current_in,
     input [9:0] left_intermediate,
     input [9:0] right_intermediate,
-    output [9:0] current_intermediate,
+    output reg [9:0] current_intermediate,
     output [9:0] sobel_X_out,
     input clk
     );
     reg [7:0] shift_add_reg [1:0];
     reg [9:0] add_reg;
-    reg [10:0] sobel_X_reg;
-    wire [8:0] shift_add_reg_0_shift;
+    reg [10:0] sobel_X_signed;
     always @(posedge clk) begin
         shift_add_reg[0] <= current_in;
         shift_add_reg[1] <= shift_add_reg[0];
-        add_reg <= current_in + shift_add_reg_0_shift + shift_add_reg[1];
-        sobel_X_reg <= right_intermediate - left_intermediate;
+        add_reg <= current_in + shift_add_reg[1];
+        current_intermediate <= add_reg + {shift_add_reg[1], 1'b0};
+        
+        sobel_X_signed <= right_intermediate - left_intermediate;
     end
-    assign shift_add_reg_0_shift = {shift_add_reg[0], 1'b0};
-    assign current_intermediate = add_reg;
-    assign sobel_X_out = (sobel_X_reg[10] == 1)?~sobel_X_reg[9:0]:sobel_X_reg[9:0];
+    assign sobel_X_out = sobel_X_signed[10]?~sobel_X_signed[9:0]+1:sobel_X_signed[9:0];
 endmodule
