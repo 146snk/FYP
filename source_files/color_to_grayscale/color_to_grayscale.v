2d765f6bd88a59fc62af27100c223faeb8dcfbf1
146snk <91668969+146snk@users.noreply.github.com>
2024-02-12 17:17:52 +0800

    finalized color_to_grayscale

--- source_files/color_to_grayscale/color_to_grayscale.v
+++ source_files/color_to_grayscale/color_to_grayscale.v
@@ -19,28 +19,22 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module color_to_grayscale(
     input [7:0] R_in,G_in,B_in,
     output [7:0] grayscale_out,
     input clk
     );
-    reg [15:0]temp_out;
-    reg [7:0]temp_carry;
-    reg [7:0]temp_sum;
-    reg [5:0]cal_R=16'b00110101;
-    reg [7:0]cal_G=16'b10010110;
-    reg [4:0]cal_B=16'b00011101;
-    reg [15:0]tempR,tempG,tempB;
+    reg [15:0] weighted_R,weighted_G,weighted_B;
+    reg [15:0] add_R_B, temp_G;
+    reg [15:0] grayscale_reg;
     always@(posedge clk)begin
-       //temp_out<=R_in*0.229+G_in*0.589+B_in*0.114;            //0.001110101,,,,0.100101101,,,,,,,,0.00011101
-     tempR=R_in*cal_R;
-     tempG=G_in*cal_G;
-     tempB=B_in*cal_B;
-     temp_out=tempR+tempB+tempG;
-//       temp_Rout<=R_in/3;
-//       temp_Gout<=G_in/3;
-//       temp_Bout<=B_in/3;    
+       //grayscale_reg<=R_in*0.229+G_in*0.589+B_in*0.114;            //0.001110101,,,,0.100101101,,,,,,,,0.00011101
+         weighted_R <= R_in*16'b00110101; // 0.229d  0.001110101b
+         weighted_G <= G_in*16'b10010110; // 0.589d  0.100101101b
+         weighted_B <= B_in*16'b00011101; // 0.114d  0.00011101b
+         add_R_B <= weighted_R + weighted_B;
+         temp_G <= weighted_G;
+         grayscale_reg <= add_R_B + weighted_G;
     end
-    assign grayscale_out=temp_out[15:8];
+    assign grayscale_out=grayscale_reg[15:8];
 endmodule
